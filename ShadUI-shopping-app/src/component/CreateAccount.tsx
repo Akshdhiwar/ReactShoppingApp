f4c386f5ab064a22475f26d2f002f39e5c9bce36
Akash Dhiwar <akashdhiwar3001@gmail.com>
2023-10-08 22:47:53 +0530

    create account screen added

---
+++ ShadUI-shopping-app/src/component/CreateAccount.tsx
@@ -0,0 +1,174 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../components/ui/form";
+import { Input } from "../components/ui/input";
+import { Button } from "../components/ui/button";
+import { Separator } from "../components/ui/separator";
+import { InstagramLogoIcon } from "@radix-ui/react-icons";
+
+const createAccountSchema = z.object({
+  name: z.string(),
+  number: z
+    .string()
+    .max(10, {
+      message: "Maximum 10 digits are allowed",
+    })
+    .min(10, {
+      message: "Minumum 10 digits are allowed",
+    }),
+  email: z.string().email(),
+  password: z
+    .string()
+    .max(15, {
+      message: "Password should not exceed 15 characters",
+    })
+    .min(8, {
+      message: "Password must be atleast 8 characters",
+    })
+    .regex(
+      new RegExp(
+        "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
+      ),
+      {
+        message:
+          "Minimum eight and maximum 15 characters, at least one uppercase letter, one lowercase letter, one number and one special character",
+      }
+    ),
+});
+
+const CreateAccount = () => {
+  const createAccountForm = useForm<z.infer<typeof createAccountSchema>>({
+    resolver: zodResolver(createAccountSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit() {}
+
+  return (
+    <>
+      <div className="flex items-center flex-col my-2">
+        <p className="text-2xl font-semibold tracking-tight mb-2">
+          Create an Account
+        </p>
+        <p className=" text-muted-foreground text-sm">
+          Enter your details to create account
+        </p>
+      </div>
+      <Form {...createAccountForm}>
+        <form onSubmit={createAccountForm.handleSubmit(onSubmit)}>
+          <FormField
+            control={createAccountForm.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Full name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Dave jones" type="text" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          ></FormField>
+          <FormField
+            control={createAccountForm.control}
+            name="number"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Phone number</FormLabel>
+                <FormControl>
+                  <Input placeholder="9876543210" type="number" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          ></FormField>
+          <FormField
+            control={createAccountForm.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="iamrobot@gmail.com"
+                    type="email"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          ></FormField>
+          <FormField
+            control={createAccountForm.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder="Password" type="password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          ></FormField>
+          <Button className="mt-4 w-full"> Create an Account </Button>
+        </form>
+      </Form>
+      {/* <div className="flex w-full items-center gap-1 text-sm my-4">
+        <Separator className="flex-1" />
+        OR CONTINUE WITH
+        <Separator className="flex-1" />
+      </div>
+      <Button variant={"outline"} className="gap-2 w-full mb-2">
+        <InstagramLogoIcon></InstagramLogoIcon> Instagram
+      </Button> */}
+      <div className="lg:hidden">
+        <div className="flex w-full items-center gap-1 text-sm my-2">
+          <Separator className="flex-1" />
+          Already have Account ?
+          <Separator className="flex-1" />
+        </div>
+        <div>
+          <Button className="w-full mt-2" variant={"outline"}>
+            Login
+          </Button>
+        </div>
+      </div>
+
+      <div className="mx-10 text-center my-1">
+        <p className="text-sm text-muted-foreground">
+          By clicking continue, you agree to our{" "}
+          <a
+            className="underline underline-offset-4 hover:text-primary"
+            href="#"
+          >
+            Terms of Service
+          </a>{" "}
+          and{" "}
+          <a
+            className="underline underline-offset-4 hover:text-primary"
+            href="#"
+          >
+            Privacy Policy
+          </a>
+          .
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default CreateAccount;
--- ShadUI-shopping-app/src/component/Login.tsx
+++ ShadUI-shopping-app/src/component/Login.tsx
@@ -14,6 +14,8 @@ import { useForm } from "react-hook-form";
 import { Input } from "../components/ui/input";
 import { Button } from "../components/ui/button";
 import { Separator } from "../components/ui/separator";
+import { useState } from "react";
+import CreateAccount from "./CreateAccount";
 
 const loginSchema = z.object({
   email: z.string().email({
@@ -39,6 +41,7 @@ const loginSchema = z.object({
 });
 
 const Login = () => {
+  const [loginScreen, setLoginScreen] = useState(true);
   const loginForm = useForm<z.infer<typeof loginSchema>>({
     resolver: zodResolver(loginSchema),
     defaultValues: {
@@ -48,6 +51,10 @@ const Login = () => {
   });
 
   function onSubmit() {}
+
+  function toggleLoginScreen() {
+    setLoginScreen((prev) => (prev = !prev));
+  }
   return (
     <div className="h-screen flex ">
       <div className="h-full w-1/2 relative box-border hidden lg:block">
@@ -71,86 +78,108 @@ const Login = () => {
         </div>
       </div>
       <div className="h-full w-full p-8 flex items-center justify-center lg:w-1/2">
-        <div className="w-[350px]">
-          <p className="text-3xl font-bold tracking-tight mb-2 text-center lg:hidden text-orange-500">
+        <Button
+          className="absolute right-8 top-8 hidden lg:block"
+          onClick={toggleLoginScreen}
+        >
+          {loginScreen ? "Create an Account" : "Login"}
+        </Button>
+        <div className="w-[350px] py-6 box-border">
+          <p className="text-3xl font-bold tracking-tight my-2 text-center lg:hidden text-orange-500">
             Dumbles.shop
           </p>
-          <div className="flex items-center flex-col my-2">
-            <p className="text-2xl font-semibold tracking-tight mb-2">Login</p>
-            <p className=" text-muted-foreground text-sm">
-              Enter your details to login
-            </p>
-          </div>
-          <div>
-            <Form {...loginForm}>
-              <form onSubmit={loginForm.handleSubmit(onSubmit)}>
-                <FormField
-                  control={loginForm.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Email</FormLabel>
-                      <FormControl>
-                        <Input
-                          placeholder="iamrobot@gmail.com"
-                          type="email"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                ></FormField>
-                <FormField
-                  control={loginForm.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Password</FormLabel>
-                      <FormControl>
-                        <Input
-                          placeholder="Enter your password "
-                          //   type="password"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                ></FormField>
-                <Button className="w-full mt-4" type="submit">
+          {loginScreen ? (
+            <div>
+              <div className="flex items-center flex-col my-2">
+                <p className="text-2xl font-semibold tracking-tight mb-2">
                   Login
+                </p>
+                <p className=" text-muted-foreground text-sm">
+                  Enter your details to login
+                </p>
+              </div>
+              <Form {...loginForm}>
+                <form onSubmit={loginForm.handleSubmit(onSubmit)}>
+                  <FormField
+                    control={loginForm.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="iamrobot@gmail.com"
+                            type="email"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  ></FormField>
+                  <FormField
+                    control={loginForm.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Enter your password "
+                            type="password"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  ></FormField>
+                  <Button className="w-full mt-4" type="submit">
+                    Login
+                  </Button>
+                </form>
+              </Form>
+              <div className="flex w-full items-center gap-1 text-sm my-4">
+                <Separator className="flex-1" />
+                or
+                <Separator className="flex-1" />
+              </div>
+              <Button variant={"outline"} className="gap-2 w-full mb-2">
+                <InstagramLogoIcon></InstagramLogoIcon>Login with Instagram
+              </Button>
+              <div className="lg:hidden">
+                <div className="flex w-full items-center gap-1 text-sm my-2">
+                  <Separator className="flex-1" />
+                  Dont have an account ?
+                  <Separator className="flex-1" />
+                </div>
+                <Button className="w-full mt-2" onClick={toggleLoginScreen}>
+                  Create an Account
                 </Button>
-              </form>
-            </Form>
-            <div className="flex w-full items-center gap-1 text-sm my-6">
-              <Separator className="flex-1" />
-              OR CONTINUE WITH
-              <Separator className="flex-1" />
+              </div>
+              <div className="mx-10 text-center my-1">
+                <p className="text-sm text-muted-foreground">
+                  By clicking continue, you agree to our{" "}
+                  <a
+                    className="underline underline-offset-4 hover:text-primary"
+                    href="#"
+                  >
+                    Terms of Service
+                  </a>{" "}
+                  and{" "}
+                  <a
+                    className="underline underline-offset-4 hover:text-primary"
+                    href="#"
+                  >
+                    Privacy Policy
+                  </a>
+                  .
+                </p>
+              </div>
             </div>
-            <Button variant={"outline"} className="gap-2 w-full mb-2">
-              <InstagramLogoIcon></InstagramLogoIcon> Instagram
-            </Button>
-            <div className="mx-10 text-center">
-              <p className="text-sm text-muted-foreground">
-                By clicking continue, you agree to our{" "}
-                <a
-                  className="underline underline-offset-4 hover:text-primary"
-                  href="#"
-                >
-                  Terms of Service
-                </a>{" "}
-                and{" "}
-                <a
-                  className="underline underline-offset-4 hover:text-primary"
-                  href="#"
-                >
-                  Privacy Policy
-                </a>
-                .
-              </p>
-            </div>
-          </div>
+          ) : (
+            <CreateAccount />
+          )}
         </div>
       </div>
     </div>
