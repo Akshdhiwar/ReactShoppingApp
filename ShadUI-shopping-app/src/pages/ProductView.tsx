fae794b1d4f42014826c1896ac5fc801cc055e93
Akash Dhiwar <akashdhiwar3001@gmail.com>
2023-12-23 13:40:48 +0530

    removed console.log

--- ShadUI-shopping-app/src/pages/ProductView.tsx
+++ ShadUI-shopping-app/src/pages/ProductView.tsx
@@ -11,11 +11,9 @@ const ProductView = () => {
     if (sessionDataString !== null) {
       const sessionData: iProduct[] = JSON.parse(sessionDataString);
       setData(sessionData);
-      console.log("in local");
     } else {
       axios.get("https://fakestoreapi.com/products").then((data) => {
         setData(data.data);
-        console.log("in axios");
         sessionStorage.setItem("products", JSON.stringify(data.data));
       });
     }
