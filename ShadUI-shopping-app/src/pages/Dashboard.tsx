b3847ad72c066ba8eadcafb795d372f63d8551e5
Akash Dhiwar <akashdhiwar3001@gmail.com>
2023-12-31 17:38:14 +0530

    updated : css for carousal

--- ShadUI-shopping-app/src/pages/Dashboard.tsx
+++ ShadUI-shopping-app/src/pages/Dashboard.tsx
@@ -75,7 +75,7 @@ const Dashboard = () => {
   }, []);
   return (
     <div className=" content-grid">
-      <div className="lg:h-screen h-[70vh] fullwidth">
+      <div className="lg:h-screen fullwidth">
         <Carousal slideImages={slideImages}></Carousal>
       </div>
 
