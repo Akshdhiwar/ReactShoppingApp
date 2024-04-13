86f9d80efb26397bb7cc172fd0af81f96ffaf23e
akash <akashdhiwar3001@gmail.com>
2024-04-13 11:53:07 +0530

    add : added custom login and removed supabase login

--- ShadUI-shopping-app/src/pages/Login.tsx
+++ ShadUI-shopping-app/src/pages/Login.tsx
@@ -1,31 +1,13 @@
 import { ChevronRightIcon, FrameIcon } from "@radix-ui/react-icons";
 import { useNavigate } from "react-router-dom";
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
-import { supabase } from "../Constants/supabase";
-import { useEffect, useState } from "react";
 import { Button } from "../components/ui/button";
+import { Label } from "../components/ui/label";
+import { Input } from "../components/ui/input";
+import { useState } from "react";
 
 const Login = () => {
   let navigate = useNavigate();
-  const [session, setSession] = useState<unknown>(null);
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
-
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-
-    if (session) {
-      navigate("/dashboard");
-    }
-
-    return () => subscription.unsubscribe();
-  }, [session]);
+  const [loginState, setLoginState] = useState(true)
   return (
     <div className="h-screen flex ">
       <div className="h-full w-1/2 relative box-border hidden lg:block">
@@ -57,20 +39,97 @@ const Login = () => {
         >
           <ChevronRightIcon height={30} width={30} />
         </Button>
-        <div className="w-[350px] py-6 box-border">
-          <p className="text-2xl font-bold leading-tight tracking-tighter md:text-3xl lg:leading-[1.1] text-orange-500">
-            Dumbles.shop
-          </p>
-          <p className=" text-base text-muted-foreground">
-            Login to get your home gym setup now.
-          </p>
-          <Auth
-            supabaseClient={supabase}
-            appearance={{ theme: ThemeSupa }}
-            providers={["google", "facebook", "apple"]}
-            socialLayout="horizontal"
-            theme="light"
-          />
+        <div className="flex items-center justify-center py-12">
+          {
+            loginState ? <div className="mx-auto grid w-[350px] gap-6">
+              <div className="grid gap-2 text-center">
+                <h1 className="text-3xl font-bold">Login</h1>
+                <p className="text-balance text-muted-foreground">
+                  Enter your email below to login to your account
+                </p>
+              </div>
+              <div className="grid gap-4">
+                <div className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                    <a
+                      href="/forgot-password"
+                      className="ml-auto inline-block text-sm underline"
+                    >
+                      Forgot your password?
+                    </a>
+                  </div>
+                  <Input id="password" type="password" required />
+                </div>
+                <Button type="submit" className="w-full">
+                  Login
+                </Button>
+                <Button variant="outline" className="w-full">
+                  Login with Google
+                </Button>
+              </div>
+              <div className="mt-4 text-center text-sm">
+                Don&apos;t have an account?{" "}
+                <a href="#" className="underline" onClick={() => setLoginState(false)}>
+                  Sign up
+                </a>
+              </div>
+            </div> :
+              <div className="mx-auto grid w-[400px] gap-6">
+                <div className="grid gap-2 text-center">
+                  <h1 className="text-3xl font-bold">Sign Up</h1>
+                  <p className="text-balance text-muted-foreground">
+                    Enter your information to create an account
+                  </p>
+                </div>
+                  <div className="grid gap-4">
+                    <div className="grid grid-cols-2 gap-4">
+                      <div className="grid gap-2">
+                        <Label htmlFor="first-name">First name</Label>
+                        <Input id="first-name" placeholder="Max" required />
+                      </div>
+                      <div className="grid gap-2">
+                        <Label htmlFor="last-name">Last name</Label>
+                        <Input id="last-name" placeholder="Robinson" required />
+                      </div>
+                    </div>
+                    <div className="grid gap-2">
+                      <Label htmlFor="email">Email</Label>
+                      <Input
+                        id="email"
+                        type="email"
+                        placeholder="m@example.com"
+                        required
+                      />
+                    </div>
+                    <div className="grid gap-2">
+                      <Label htmlFor="password">Password</Label>
+                      <Input id="password" type="password" />
+                    </div>
+                    <Button type="submit" className="w-full">
+                      Create an account
+                    </Button>
+                    <Button variant="outline" className="w-full">
+                      Sign up with GitHub
+                    </Button>
+                  </div>
+                  <div className="mt-4 text-center text-sm">
+                    Already have an account?{" "}
+                    <a href="#" className="underline" onClick={() => setLoginState(true)}>
+                      Sign in
+                    </a>
+                  </div>
+              </div>
+          }
         </div>
       </div>
     </div>
