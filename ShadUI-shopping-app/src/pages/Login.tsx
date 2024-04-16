4718a958f8eaef944e7a80ec6d0981583d2fd847
akash <akashdhiwar3001@gmail.com>
2024-04-16 16:28:52 +0530

    remove : custom login screen and added supabase login screen

--- ShadUI-shopping-app/src/pages/Login.tsx
+++ ShadUI-shopping-app/src/pages/Login.tsx
@@ -1,66 +1,89 @@
 import { ChevronRightIcon, FrameIcon } from "@radix-ui/react-icons";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../components/ui/button";
-import { Input } from "../components/ui/input";
-import { useState } from "react";
-import axios from "axios";
-import { baseURL } from "../Constants/api";
-import z from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../components/ui/form";
+// import { Input } from "../components/ui/input";
+import { useEffect, useState } from "react";
+// import axios from "axios";
+// import { baseURL } from "../Constants/api";
+// import z from "zod"
+// import { zodResolver } from "@hookform/resolvers/zod"
+// import { useForm } from "react-hook-form";
+// import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../components/ui/form";
+import { supabase } from "../Constants/supabase";
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
 
-const loginSchema = z.object({
-  email: z.string().email("Valid email is required"),
-  password: z.string().min(8, "Minimum 8 characters are required")
-})
 
-const signUpSchema = z.object({
-  email: z.string().email("Valid email is required"),
-  password: z.string().min(8, "Minimum 8 characters are required"),
-  firstName: z.string(),
-  lastName: z.string()
-})
+// const loginSchema = z.object({
+//   email: z.string().email("Valid email is required"),
+//   password: z.string().min(8, "Minimum 8 characters are required")
+// })
+
+// const signUpSchema = z.object({
+//   email: z.string().email("Valid email is required"),
+//   password: z.string().min(8, "Minimum 8 characters are required"),
+//   firstName: z.string(),
+//   lastName: z.string()
+// })
 
 const Login = () => {
   let navigate = useNavigate();
-  const [loginState, setLoginState] = useState(true)
+  const [session, setSession] = useState<unknown>(null);
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
 
-  const loginForm = useForm<z.infer<typeof loginSchema>>({
-    resolver: zodResolver(loginSchema)
-  })
-  const signUpForm = useForm<z.infer<typeof signUpSchema>>({
-    resolver: zodResolver(signUpSchema)
-  })
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    if (session) {
+      navigate("/dashboard");
+    }
+
+    return () => subscription.unsubscribe();
+  }, [session]);
+
+  // const [loginState, setLoginState] = useState(true)
+
+  // const loginForm = useForm<z.infer<typeof loginSchema>>({
+  //   resolver: zodResolver(loginSchema)
+  // })
+  // const signUpForm = useForm<z.infer<typeof signUpSchema>>({
+  //   resolver: zodResolver(signUpSchema)
+  // })
   
-  const onLoginSubmit = async (values: z.infer<typeof loginSchema>) => {
-    try {
-      const response = await axios.post(`${baseURL}account/login`, {
-        "Email": values.email,
-        "Password": values.password
-      });
-      const accessToken = response.data.access_token;
-      const expirationTime = 3600 * 1000; // 1 hour in milliseconds
-      const expires = new Date(Date.now() + expirationTime).toUTCString();
-      document.cookie = `access_token=${accessToken}; expires=${expires}; path=/`;
-      navigate("/dashboard")
-    } catch (error) {
-      console.error('Login API error:', error);
-    }
-  }
+  // const onLoginSubmit = async (values: z.infer<typeof loginSchema>) => {
+  //   try {
+  //     const response = await axios.post(`${baseURL}account/login`, {
+  //       "Email": values.email,
+  //       "Password": values.password
+  //     });
+  //     const accessToken = response.data.access_token;
+  //     const expirationTime = 3600 * 1000; // 1 hour in milliseconds
+  //     const expires = new Date(Date.now() + expirationTime).toUTCString();
+  //     document.cookie = `access_token=${accessToken}; expires=${expires}; path=/`;
+  //     navigate("/dashboard")
+  //   } catch (error) {
+  //     console.error('Login API error:', error);
+  //   }
+  // }
 
-  const onSignUpSubmit = async (values: z.infer<typeof signUpSchema>) => {
-    try {
-      await axios.post(`${baseURL}account/signup`, {
-        "FirstName": values.firstName,
-        "LastName": values.lastName,
-        "Email": values.email,
-        "Password": values.password,
-      });
-    } catch (error) {
-      console.error('Signup API error:', error);
-    }
-  };
+  // const onSignUpSubmit = async (values: z.infer<typeof signUpSchema>) => {
+  //   try {
+  //     await axios.post(`${baseURL}account/signup`, {
+  //       "FirstName": values.firstName,
+  //       "LastName": values.lastName,
+  //       "Email": values.email,
+  //       "Password": values.password,
+  //     });
+  //   } catch (error) {
+  //     console.error('Signup API error:', error);
+  //   }
+  // };
 
   return (
     <div className="h-screen flex ">
@@ -93,7 +116,7 @@ const Login = () => {
         >
           <ChevronRightIcon height={30} width={30} />
         </Button>
-        <div className="flex items-center justify-center py-12">
+        {/* <div className="flex items-center justify-center py-12">
           {
             loginState ? <div className="mx-auto grid w-[350px] gap-6">
               <div className="grid gap-2 text-center">
@@ -222,6 +245,21 @@ const Login = () => {
                 </div>
               </div>
           }
+        </div> */}
+         <div className="w-[350px] py-6 box-border">
+          <p className="text-2xl font-bold leading-tight tracking-tighter md:text-3xl lg:leading-[1.1] text-orange-500">
+            Dumbles.shop
+          </p>
+          <p className=" text-base text-muted-foreground">
+            Login to get your home gym setup now.
+          </p>
+          <Auth
+            supabaseClient={supabase}
+            appearance={{ theme: ThemeSupa }}
+            providers={["google", "facebook", "apple"]}
+            socialLayout="horizontal"
+            theme="light"
+          />
         </div>
       </div>
     </div>
