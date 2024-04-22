499f375edd44b7bec0b53a5c1f580eaa843933bd
akash <akashdhiwar3001@gmail.com>
2024-04-22 20:53:30 +0530

    add : remove and add quantity api in quantity component

--- ShadUI-shopping-app/src/components/ProductDetails/Quantity.tsx
+++ ShadUI-shopping-app/src/components/ProductDetails/Quantity.tsx
@@ -3,6 +3,9 @@ import { Button } from "../ui/button";
 import React, { useContext } from "react";
 import iProduct from "../../Interfaces/Products";
 import { CartContext } from "../../Context/CartContext";
+import { UserContext } from "../../Context/UserContext";
+import { useToast } from "../ui/use-toast";
+import axiosHttp from "../../axiosHandler/axiosHandler";
 // import { Input } from "../ui/input";
 
 interface QuantityProps {
@@ -11,12 +14,50 @@ interface QuantityProps {
 
 const Quantity: React.FC<QuantityProps> = ({ product }) => {
   const cart = useContext(CartContext);
+  const user = useContext(UserContext);
+  const {toast} = useToast();
 
   // Find the quantity of the current product in the cart
   let quantity: number | undefined = 1; // Default quantity
-  const productInCart = cart?.cart.find((ele) => ele.ID === product?.ID);
+  const productInCart : any = cart?.cart.find((ele:any) => ele.Product.ID === product?.ID);
   if (productInCart) {
-    quantity = productInCart.quantity;
+    quantity = productInCart.Quantity;
+  }
+
+  async function addQuantity(id: string) {
+
+    const payload = {
+      "UserID": user?.user?.sub
+    }
+
+    await axiosHttp.post(`cart/inc/${id}`, payload).then(() => {
+      toast({
+        title: "Success",
+        description: "Added Quantity"
+      })
+
+      cart?.addQuantity(id);
+    }).catch((error)=>{
+      console.error(error)
+    })
+  }
+
+  async function removeQuantity(id: string) {
+
+    const payload = {
+      "UserID": user?.user?.sub
+    }
+
+    await axiosHttp.post(`cart/dec/${id}`, payload).then(() => {
+      toast({
+        title: "Success",
+        description: "Removed Quantity"
+      })
+
+      cart?.removeQuantity(id);
+    }).catch((error)=>{
+      console.error(error)
+    })
   }
 
   return (
@@ -25,12 +66,12 @@ const Quantity: React.FC<QuantityProps> = ({ product }) => {
         <Button
           variant={"outline"}
           disabled={quantity === 1}
-          onClick={() => cart?.removeQuantity(product!)}
+          onClick={() => removeQuantity(product!.ID)}
         >
           <MinusIcon />
         </Button>
         <p className="flex-1 text-center">Qty : {quantity}</p>
-        <Button variant={"outline"} onClick={() => cart?.addQuantity(product!)}>
+        <Button variant={"outline"} onClick={() => addQuantity(product!.ID)}>
           <PlusIcon />
         </Button>
       </div>
