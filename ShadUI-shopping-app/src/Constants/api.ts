1488d95575e14c573883032261f004b7f2e38167
akash <akashdhiwar3001@gmail.com>
2024-04-29 23:36:09 +0530

    add : some normal updates

--- ShadUI-shopping-app/src/Constants/api.ts
+++ ShadUI-shopping-app/src/Constants/api.ts
@@ -1,2 +1,2 @@
 
-export const baseURL = import.meta.env.VITE_ENVIRONMENT === "PROD" ? "http://localhost:3000/api/v1/" : "https://go-backend.up.railway.app/api/v1/"
+export const baseURL = import.meta.env.VITE_ENVIRONMENT === "Local" ? "http://localhost:3000/api/v1/" : "https://go-backend.up.railway.app/api/v1/"
--- ShadUI-shopping-app/src/Constants/supabase.ts
+++ ShadUI-shopping-app/src/Constants/supabase.ts
@@ -1,8 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-let url = import.meta.env.VITE_ENVIRONMENT === "Local" ? import.meta.env.VITE_SUPABASE_URL : import.meta.env.VITE_SUPABASE_URL_PROD
-let anon = import.meta.env.VITE_ENVIRONMENT === "Local" ? import.meta.env.VITE_SUPABASE_ANON_KEY : import.meta.env.VITE_SUPABASE_ANON_KEY_PROD
-console.log(url,anon);
 export const supabase = createClient(
   import.meta.env.VITE_ENVIRONMENT === "Local" ? import.meta.env.VITE_SUPABASE_URL : import.meta.env.VITE_SUPABASE_URL_PROD,
   import.meta.env.VITE_ENVIRONMENT === "Local" ? import.meta.env.VITE_SUPABASE_ANON_KEY : import.meta.env.VITE_SUPABASE_ANON_KEY_PROD,
